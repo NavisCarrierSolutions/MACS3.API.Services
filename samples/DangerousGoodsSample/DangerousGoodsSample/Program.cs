b80d105287b75ca0b3a214a5aa093201175f1962
the-unforgiven666 <40465707+the-unforgiven666@users.noreply.github.com>
2019-03-15 14:02:46 +0100

    Add files via upload

---
+++ samples/DangerousGoodsSample/DangerousGoodsSample/Program.cs
@@ -0,0 +1,123 @@
+﻿using MACS3.Connected.SDK.Core;
+using MACS3.Connected.SDK.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DangerousGoodsSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(async () =>
+            {
+                var apiKey = "<YOUR-API-KEY>";
+                var imoNumber = 1234567;
+
+                var provider = new ApiKeyProvider {ApiKey = apiKey};
+
+                using (var apiClient = await MACS3.Connected.SDK.DangerousGoods.API2.CreateClientAsync(provider))
+                {
+                    try
+                    {
+                        DaGoCheckParameters parameter = new DaGoCheckParameters();
+
+                        parameter.Settings = new DaGoSettingsParameter(
+                            amendmentNumber: 38,
+                            checkForSunlight: true,
+                            stowKeepCoolCargoAwayFromReefers: true,
+                            stowKeepCoolCargoAwayFromSun: true);
+
+                        parameter.Containers = new List<DaGoContainerParameter>
+                        {
+                            new DaGoContainerParameter
+                            (
+                                id: "11",
+                                containerId: "NCVS1234568",
+                                typeIsoCode: "42G0",
+                                grossWeight: 10.1,
+                                position: "020206",
+                                relativeVcg: 0.50,
+                                height: 2.591,
+                                length: 12.192,
+                                width: 2.438,
+                                openTop: false,
+                                liveReefer: false,
+                                empty: false,
+                                doorOrientationAft: false,
+                                foodStuff: false,
+                                sunExposed: false,
+                                dangerousGoods: new List<DangerousGoodParameter>
+                                {
+                                    new DangerousGoodParameter
+                                    (
+                                        un: 1234,
+                                        _class: "3",
+                                        packingGroup: 2,
+                                        subLabel: "",
+                                        subLabel2: "",
+                                        stowageCategory: "E",
+                                        technicalName: "",
+                                        flashpoint: -28,
+                                        limitedQuantity: false,
+                                        exceptedQuantity: false,
+                                        marinePollutant: false,
+                                        caa: false,
+                                        segregationGroup: ""
+                                    )
+                                }
+                            ),
+                            new DaGoContainerParameter
+                            (
+                                id: "12",
+                                containerId: "NCVS1234569",
+                                typeIsoCode: "42G0",
+                                grossWeight: 10.1,
+                                position: "020208",
+                                relativeVcg: 0.50,
+                                height: 2.591,
+                                length: 12.192,
+                                width: 2.438,
+                                openTop: false,
+                                liveReefer: false,
+                                empty: false,
+                                doorOrientationAft: false,
+                                setPoint: 0,
+                                foodStuff: false,
+                                sunExposed: false,
+                                dangerousGoods: new List<DangerousGoodParameter>
+                                {
+                                    new DangerousGoodParameter
+                                    (
+                                        un: 4,
+                                        _class: "1.1D",
+                                        packingGroup: 1,
+                                        subLabel: "",
+                                        subLabel2: "",
+                                        stowageCategory: "4",
+                                        technicalName: "",
+                                        limitedQuantity: false,
+                                        exceptedQuantity: false,
+                                        marinePollutant: false,
+                                        caa: false,
+                                        segregationGroup: ""
+                                    )
+                                }
+                            )
+                        };
+
+                        parameter.ContainerToCheck = new List<ContainerIdentifier>
+                        {
+                            new ContainerIdentifier(id: "11", containerId: "NCVS1234568", position: "020206")
+                        };
+
+                        DaGoCheckResult result = await apiClient.PerformDangerousGoodsCheckAsync(imoNumber, parameter);
+                    }
+                    catch (ApiException e)
+                    {
+                    }
+                }
+            }).Wait();
+        }
+    }
+}
